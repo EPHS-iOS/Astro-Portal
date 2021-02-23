6e5da518c94d8f4b420789bfc2182fab165220cb
kanishk <kanishkd04@gmail.com>
2021-02-23 11:53:28 -0600

    player

--- FastShooterGame/GameScene.swift
+++ FastShooterGame/GameScene.swift
@@ -13,15 +13,16 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
-    var player = SKSpriteNode()
+    var player = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: 100))
     
     override func didMove(to view: SKView) {
         
-        player = SKSpriteNode()
+//        player = SKSpriteNode()
         player.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        player.position = CGPoint(x:0, y: -200)
+        player.position = CGPoint(x:0, y: 0)
         player.zPosition = 2
-        player.physicsBody = SKPhysicsBody(texture: player.texture!, alphaThreshold: 0.7, size: (player.texture!.size()))
+        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 100))
+//        player.physicsBody = SKPhysicsBody(texture: player.texture!, alphaThreshold: 0.7, size: (player.texture!.size()))
         player.physicsBody?.affectedByGravity = false
 //        player.physicsBody?.categoryBitMask = PhysicsCategories.player
 //        player.physicsBody?.collisionBitMask = PhysicsCategories.None

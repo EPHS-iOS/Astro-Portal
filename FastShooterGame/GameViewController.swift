fe0602d007233a6c94e45899484d436b83f048b8
ViswaNadimpalli <64009170@ep-student.org>
2021-02-24 11:21:25 -0600

    added view.showsPhysics

--- FastShooterGame/GameViewController.swift
+++ FastShooterGame/GameViewController.swift
@@ -24,6 +24,7 @@ class GameViewController: UIViewController {
                 view.presentScene(scene)
             }
             
+            view.showsPhysics = true
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true

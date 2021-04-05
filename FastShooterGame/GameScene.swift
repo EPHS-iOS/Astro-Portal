c9a9f3beebb07199ef3839843559c6d19f3129f9
Sai Guntabonu <saiguntabonu@gmail.com>
2021-04-05 11:16:59 -0500

    Revert "Revert "lazer and map change""
    This reverts commit 5bab5ff6dedab320c7d6914d672b5af646f5c969.

--- FastShooterGame/GameScene.swift
+++ FastShooterGame/GameScene.swift
@@ -13,17 +13,27 @@ class GameScene: SKScene {
   
     var cameraNode: SKCameraNode?
     var player : SKSpriteNode?
+    var bulletDuration = 3.0
+    var gun1 : SKSpriteNode?
     let right = SKSpriteNode(imageNamed: "right")
     let left = SKSpriteNode(imageNamed: "left")
     let jump = SKSpriteNode(imageNamed: "jump")
-    
+    struct PhysicsCategory {
+      static let none      : UInt32 = 0
+      static let all       : UInt32 = UInt32.max
+      static let bullet   : UInt32 = 0b1       // 1
+        static let player : UInt32 = 0b10
+        
+    }
     override func didMove(to view: SKView) {
         player = childNode(withName: "player") as?SKSpriteNode
         cameraNode = childNode(withName: "camera") as?SKCameraNode
-       
+       gun1 = childNode(withName: "gun wall") as?SKSpriteNode
+     
+        
 //        player = SKSpriteNode()
         
-        player?.position = CGPoint(x:0, y: 0)
+      
        player?.zPosition = 10
      //  player?.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 100, height: 100))
 //        player.physicsBody = SKPhysicsBody(texture: player.texture!, alphaThreshold: 0.7, size: (player.texture!.size()))
@@ -35,39 +45,70 @@ class GameScene: SKScene {
         //self.addChild(player!)
         right.position = CGPoint(x: self.size.width * -0.2, y: self.size.height * -0.4)
         right.zPosition = 3
-        right.size=CGSize(width: 130,height: 100)
+        right.size=CGSize(width:self.size.width/4,height:self.size.height/4)
         right.alpha = 0.8
         self.addChild(right)
         
         
         left.position = CGPoint(x: self.size.width * -0.3, y: self.size.height * -0.4)
         left.zPosition = 3
-        left.size=CGSize(width: 130,height: 100)
+        left.size=CGSize(width:self.size.width/4,height:self.size.height/4)
         left.alpha = 0.8
         self.addChild(left)
         
         
         jump.position = CGPoint(x: self.size.width * 0.3, y: self.size.height * -0.4)
         jump.zPosition = 3
-       jump.size=CGSize(width: 70,height: 100)
+       jump.size=CGSize(width: self.size.width/5,height:self.size.height/3)
         jump.alpha = 0.8
         self.addChild(jump)
       
         self.physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
- 
+        run(SKAction.repeatForever(
+          SKAction.sequence([
+            SKAction.run(addBullet),
+            SKAction.wait(forDuration:bulletDuration)
+          ])
+        ))
+
+        
+          
     }
     func centerOnNode(node:SKNode){
         self.camera!.run(SKAction.move(to: CGPoint(x:node.position.x, y:node.position.y), duration: 0.3))
-        self.right.run(SKAction.move(to: CGPoint(x:node.position.x-250, y:node.position.y-200), duration: 0.3))
-        self.left.run(SKAction.move(to: CGPoint(x:node.position.x-400, y:node.position.y-200), duration: 0.3))
-        self.jump.run(SKAction.move(to: CGPoint(x:node.position.x+400, y:node.position.y-200), duration: 0.3))
+        self.right.run(SKAction.move(to: CGPoint(x:node.position.x-450, y:node.position.y-300), duration: 0.3))
+        self.left.run(SKAction.move(to: CGPoint(x:node.position.x-700, y:node.position.y-300), duration: 0.3))
+       self.jump.run(SKAction.move(to: CGPoint(x:node.position.x+400, y:node.position.y-300), duration: 0.3))
+       
     }
     override func didFinishUpdate() {
     centerOnNode(node: player!)
        
         
     }
-    
+    func random() -> CGFloat {
+      return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+      return random() * (max - min) + min
+    }
+    func addBullet(){
+        let ran = random(min: -100, max: 100)
+        let location = CGPoint(x:gun1!.position.x,y:gun1!.position.y+ran)
+        let bullet = SKSpriteNode(imageNamed: "laser")
+        bullet.physicsBody = SKPhysicsBody(circleOfRadius: 25)
+        bullet.physicsBody?.isDynamic = false // 2
+        bullet.physicsBody?.categoryBitMask = PhysicsCategory.bullet // 3
+       bullet.physicsBody?.contactTestBitMask = PhysicsCategory.none // 4
+       bullet.physicsBody?.collisionBitMask = PhysicsCategory.none // 5
+        bullet.size = CGSize(width:50,height:50)
+        bullet.position=CGPoint(x:gun1!.position.x,y:gun1!.position.y-150+ran)
+        addChild(bullet)
+        let duration = random(min:CGFloat(10), max:CGFloat(15))
+        let actionMove = SKAction.move(to: CGPoint(x: -10000, y:gun1!.position.y-150+ran), duration:TimeInterval(duration))
+        let moveDone = SKAction.removeFromParent()
+        bullet.run(SKAction.sequence([actionMove,moveDone]))
+    }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
             for touch: AnyObject in touches{

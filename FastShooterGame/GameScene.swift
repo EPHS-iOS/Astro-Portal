99b05dde5b084a1ee6531ebd3cddcefef0ed41bf
Sai Guntabonu <saiguntabonu@gmail.com>
2021-04-12 11:09:09 -0500

    physicsInCode

--- FastShooterGame/GameScene.swift
+++ FastShooterGame/GameScene.swift
@@ -18,18 +18,47 @@ class GameScene: SKScene {
     let right = SKSpriteNode(imageNamed: "right")
     let left = SKSpriteNode(imageNamed: "left")
     let jump = SKSpriteNode(imageNamed: "jump")
+   
     struct PhysicsCategory {
       static let none      : UInt32 = 0
       static let all       : UInt32 = UInt32.max
       static let bullet   : UInt32 = 0b1       // 1
         static let player : UInt32 = 0b10
+        static let map : UInt32 = 0b11
+        
         
     }
     override func didMove(to view: SKView) {
+        
+        physicsWorld.contactDelegate = self
+        
         player = childNode(withName: "player") as?SKSpriteNode
         cameraNode = childNode(withName: "camera") as?SKCameraNode
        gun1 = childNode(withName: "gun wall") as?SKSpriteNode
-     
+       let tileMap = (self.childNode(withName: "Tile Map Node") as? SKTileMapNode)!
+        let tileSize = tileMap.tileSize
+        let halfWidth = CGFloat(tileMap.numberOfColumns) / 2.0 * tileSize.width
+        let halfHeight = CGFloat(tileMap.numberOfRows) / 2.0 * tileSize.height
+
+        for col in 0..<tileMap.numberOfColumns {
+            for row in 0..<tileMap.numberOfRows {
+                let tileDefinition = tileMap.tileDefinition(atColumn: col, row: row)
+                let isCobblestone = tileDefinition?.userData?["Cobblestone"] as? Bool
+                if (isCobblestone ?? false) {
+                    let x = CGFloat(col) * tileSize.width - halfWidth
+                    let y = CGFloat(row) * tileSize.height - halfHeight
+                    let rect = CGRect(x: 0, y: 0, width: tileSize.width, height: tileSize.height)
+                    let tileNode = SKShapeNode(rect: rect)
+                    tileNode.position = CGPoint(x: x, y: y)
+                    tileNode.physicsBody = SKPhysicsBody.init(rectangleOf: tileSize, center: CGPoint(x: tileSize.width / 2.0, y: tileSize.height / 2.0))
+                    tileNode.physicsBody?.isDynamic = false
+                    tileNode.alpha=0
+                    tileNode.physicsBody?.categoryBitMask = PhysicsCategory.map
+                    tileNode.physicsBody?.contactTestBitMask = PhysicsCategory.player
+                    tileMap.addChild(tileNode)
+                }
+            }
+        }
         
 //        player = SKSpriteNode()
         
@@ -42,6 +71,7 @@ class GameScene: SKScene {
 //        player.physicsBody?.collisionBitMask = PhysicsCategories.None
 //        player.physicsBody?.contactTestBitMask = PhysicsCategories.rock
         player?.setScale(1) //75
+        player?.physicsBody?.categoryBitMask=PhysicsCategory.player
         //self.addChild(player!)
         right.position = CGPoint(x: self.size.width * -0.2, y: self.size.height * -0.4)
         right.zPosition = 3
@@ -99,11 +129,11 @@ class GameScene: SKScene {
         bullet.physicsBody = SKPhysicsBody(circleOfRadius: 25)
         bullet.physicsBody?.isDynamic = false // 2
         bullet.physicsBody?.categoryBitMask = PhysicsCategory.bullet // 3
-       bullet.physicsBody?.contactTestBitMask = PhysicsCategory.none // 4
+       bullet.physicsBody?.contactTestBitMask = PhysicsCategory.player // 4
        bullet.physicsBody?.collisionBitMask = PhysicsCategory.none // 5
         bullet.size = CGSize(width:50,height:50)
         bullet.position=CGPoint(x:gun1!.position.x,y:gun1!.position.y-150+ran)
-        addChild(bullet)
+     addChild(bullet)
         let duration = random(min:CGFloat(10), max:CGFloat(15))
         let actionMove = SKAction.move(to: CGPoint(x: -10000, y:gun1!.position.y-150+ran), duration:TimeInterval(duration))
         let moveDone = SKAction.removeFromParent()
@@ -202,4 +232,28 @@ class GameScene: SKScene {
 }
     
    
-
+extension GameScene: SKPhysicsContactDelegate {
+  func didBegin(_ contact: SKPhysicsContact) {
+    print("check")
+    // 1
+    var firstBody: SKPhysicsBody
+    var secondBody: SKPhysicsBody
+    if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+      firstBody = contact.bodyA
+      secondBody = contact.bodyB
+    } else {
+      firstBody = contact.bodyB
+      secondBody = contact.bodyA
+    }
+    
+    // 2
+    if ((firstBody.categoryBitMask & PhysicsCategory.player != 0) &&
+      (secondBody.categoryBitMask & PhysicsCategory.map != 0)) {
+        print("yes")
+        if let map = firstBody.node as? SKSpriteNode,
+        let player = secondBody.node as? SKSpriteNode {
+        print("yes")
+      }
+    }
+  }
+}

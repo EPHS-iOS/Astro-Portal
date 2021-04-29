c18448c67266aa498caa8c6679545daaed93b709
kanishk <kanishkd04@gmail.com>
2021-04-29 11:00:13 -0500

    Revert "lev3and4"
    This reverts commit d40c15e4ef0124c0db0fee7484f6ba560d2591fd.

--- FastShooterGame/level2.swift
+++ FastShooterGame/level2.swift
@@ -1,14 +1,14 @@
 //
-//  level4.swift
+//  level2.swift
 //  Platformer
 //
-//  Created by 64004018 on 4/22/21.
+//  Created by 64004018 on 4/20/21.
 //
 
 import UIKit
 import SpriteKit
 import GameplayKit
 
-class level4: SKScene {
+class level2: SKScene {
 
 }
